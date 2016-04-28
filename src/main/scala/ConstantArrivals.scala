2bed46b210b9ab57c737c7f06444eeeab826fb97
Brenton Walker <brenton>
2016-04-28 13:37:15 +0200

    change simple program to submit small batches of tasks at fixed intervals

--- src/main/scala/ConstantArrivals.scala
+++ src/main/scala/ConstantArrivals.scala
@@ -5,19 +5,36 @@ import org.apache.spark.SparkConf
 import scala.math.random
 
 object ConstantArrivals {
+  
+  def runPiSlices(spark:SparkContext, n: Int, slices:Int): Long = {
+    println("*** runPiSlices( "+n+" , "+slices+" )")
+    val count = spark.parallelize(1 until n, slices).map { i =>
+      val x = random * 2 - 1
+      val y = random * 2 - 1
+      if (x*x + y*y < 1) 1 else 0
+    }.reduce(_ + _)
+    return count
+  }
+  
 	def main(args: Array[String]) {
 		val conf = new SparkConf().setMaster("local[1]").setAppName("Spark Pi")
 		println("*** got conf ***")
 		val spark = new SparkContext(conf)
 		println("*** got spark context ***")
-		val slices = if (args.length > 0) args(0).toInt else 2
-		val n = math.min(100000L * slices, Int.MaxValue).toInt
-		val count = spark.parallelize(1 until n, slices).map { i =>
-      val x = random * 2 - 1
-      val y = random * 2 - 1
-      if (x*x + y*y < 1) 1 else 0
-    }.reduce(_ + _)
-    println("Pi is roughly " + 4.0 * count / n)
+		val totalSlices = if (args.length > 0) args(0).toInt else 2
+		println("*** totalSlices = "+totalSlices+" ***")
+		val totalN = math.min(100000L * totalSlices, Int.MaxValue).toInt
+		val slicesPerStep = 2
+		val iterationsPerSlice = totalN/totalSlices
+		var count: Long = 0
+		var slicesRun = 0
+		while (slicesRun < totalSlices) {
+		  var s = math.min(slicesPerStep, totalSlices-slicesRun)
+		  count += runPiSlices(spark, iterationsPerSlice*s, s)
+		  slicesRun += s
+		  Thread sleep 1000
+		}
+		println("Pi is roughly " + 4.0 * count / totalN)
     spark.stop()
 	}
 }
